8a6551b47a2faeae8596bcc7b31e07aa1507c299
Michal Mokran <M.Mokran@student.tudelft.nl>
2021-02-24 00:19:43 +0100

    Formatting

--- Raspberry_Pi/turret_i2c.py
+++ Raspberry_Pi/turret_i2c.py
@@ -3,20 +3,21 @@
 
 from smbus2 import SMBus
 
+
 class i2c_data:
-    address   = 0
-    panSpeed  = 0
+    address = 0
+    panSpeed = 0
     tiltSpeed = 0
-    panDir    = 0
-    tiltDir   = 0
-    calibrate   = 0
+    panDir = 0
+    tiltDir = 0
+    calibrate = 0
     laserPower = 0
     laserOn = 0
     bus = None
-    
+
     def __init__(self, address):
         self.address = address
-        self.bus = SMBus(1) # indicates /dev/ic2-1
+        self.bus = SMBus(1)  # indicates /dev/ic2-1
 
     def send_data(self):
         data = [self.panSpeed, self.tiltSpeed, self.panDir, self.tiltDir, self.calibrate, self.laserPower, self.laserOn]
@@ -24,25 +25,24 @@ class i2c_data:
             self.bus.write_i2c_block_data(self.address, 0x00, data)
         except OSError:
             print('Codefarted')
-        
+
     def set_panSpeed(self, panSpeed):
         self.panSpeed = panSpeed
-        
+
     def set_tiltSpeed(self, tiltSpeed):
         self.tiltSpeed = tiltSpeed
-        
+
     def set_panDir(self, panDir):
         self.panDir = panDir
-        
+
     def set_tiltDir(self, tiltDir):
         self.tiltDir = tiltDir
-        
+
     def set_calibrate(self, calibrate):
         self.calibrate = calibrate
-        
+
     def set_laserPower(self, laserPower):
         self.laserPower = laserPower
-        
+
     def set_laserOn(self, laserOn):
         self.laserOn = laserOn
-
