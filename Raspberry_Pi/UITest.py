c1d81c594b6fc7449799c819be9ca30f72daefb8
cinbarker <cinbarker@gmail.com>
2022-02-15 20:18:58 +0100

    change application name

--- Raspberry_Pi/UITest.py
+++ Raspberry_Pi/UITest.py
@@ -1,4 +1,6 @@
 from PyQt5.QtGui import QIcon
+from PyQt5.QtWidgets import QApplication
+
 import PyUi
 import PyQt5
 from PyQt5 import QtWidgets
@@ -9,12 +11,15 @@ if __name__ == "__main__":
     import sys
     import platform
     if platform.system() == 'Darwin':
-        pass
         os.environ['QT_MAC_WANTS_LAYER'] = '1'  # added to fix operation on mac
-    app = QtWidgets.QApplication(sys.argv)
-    app.setWindowIcon(QIcon(os.path.join(os.path.dirname(sys.modules[__name__].__file__), 'icon.png')))
-    ui = PyUi.Ui_MainWindow()
+    app = QApplication(sys.argv)
+    app.setApplicationName("OpenTurret")
+    app.setWindowIcon(QIcon(os.path.join(os.path.dirname(sys.modules[__name__].__file__), 'icon.png')))  # Add Icon
+
+
     # app.setStyle("fusion")
+
+    ui = PyUi.Ui_MainWindow()
     gui = MyWindow(ui)
     ui.setupUi(gui)
     gui.startup_procedure()  # Startup app procedure
